ec6d54a9abab92a3980de1187bf81f52682686e0
weweaaa <a0926422280@gmail.com>
2020-07-07 17:32:05 +0800

    新增：實作封裝 Material Input 元件的 Child Custom FormControl Component [註解版]   - 可能會出現此錯誤訊息的版本 [ ERROR Error: ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked ]

--- projects/libs/src/lib/form-manager/form-manager.module.ts
+++ projects/libs/src/lib/form-manager/form-manager.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormManagerComponent } from './form-manager.component';
+import { KeydownInputComponent } from './type/keydown-input/keydown-input.component';
 
 @NgModule({
-  declarations: [FormManagerComponent],
+  declarations: [FormManagerComponent, KeydownInputComponent],
   exports: [FormManagerComponent],
   imports: [
     CommonModule,
